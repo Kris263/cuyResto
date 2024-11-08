3526cdf4c860ada7203cbb77c3523ca7efdc358a
Kris263 <kristianptr2633@gmail.com>
2024-11-09 00:23:20 +0800

    Ada yang diupdate

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+include "services/koneksi.php";
+define("APP_NAME", "CUYRESTO - WEBSITE PENERIMAAN TAMU");
+
+$query_meja = "SELECT * FROM meja";
+$result_meja = $conn -> query($query_meja);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title><?php echo APP_NAME?></title>
+</head>
+
+<body>
+    <h1 align="center">DAFTAR MEJA</h1>
+    <div class="container">
+        <?php 
+                foreach ($result_meja as $meja) {
+            ?>
+        <div class="card" onclick="goToMeja(`<?= $meja['no_meja'] ?>` , `<?= $meja['nama_pelanggan'] ?>`)">
+            <b><?php echo $meja['tipe_meja'] . " " . $meja['no_meja']?></b>
+            <p>
+                <?php 
+                    if($meja['nama_pelanggan'] == NULL AND $meja['jumlah_orang'] == NULL) {
+                        echo "Not Reserved";
+                    } else {
+                        echo $meja['nama_pelanggan'] . " " . $meja['jumlah_orang'] . " orang";
+                    }
+                ?>
+            </p>
+        </div>
+        <?php } ?>
+
+    </div>
+
+    <script>
+    function goToMeja(no_meja, nama_pelanggan) {
+        const url = "meja.php";
+        const params = `?no_meja = ${no_meja} & nama_pelanggan = ${nama_pelanggan}`
+        window.location.replace(url + params)
+    }
+    </script>
+</body>
+
+</html>
