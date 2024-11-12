0c17741651c74d2277c971aa5fc08da78e545a63
Kris263 <kristianptr2633@gmail.com>
2024-11-12 23:16:14 +0800

    update in meja

--- meja.php
+++ meja.php
@@ -1,5 +1,41 @@
 <?php 
-define("APP_NAME", "BOOKING MEJA")
+include "services/koneksi.php";
+session_start();
+if(isset($_SESSION['isLogin']) && $_SESSION['isLogin'] == false) {
+    header("location: login.php");
+}
+define("APP_NAME", "NOMOR MEJA ");
+
+
+$no_meja = "";
+$nama_pelanggan = "";
+$update_notifikasi = "";
+
+if(isset($_GET['no_meja']) && $_GET['no_meja'] !== "") {
+    $no_meja = $_GET['no_meja'];
+} else {
+    echo "Meja tidak ditemukan";
+}
+
+if(isset($_GET['nama_pelanggan']) && $_GET['nama_pelanggan'] !== "") {
+    $nama_pelanggan = $_GET['nama_pelanggan'];
+    header("location: finish.php?no_meja=$no_meja&nama_pelanggan=$nama_pelanggan");
+}
+
+if (isset($_POST['btn-pesan'])) {
+    $nama_pelanggan = $_POST['nama_pelanggan'];
+    $jumlah_orang = $_POST['jumlah_orang'];
+
+    $sql_update = "UPDATE meja SET nama_pelanggan = '$nama_pelanggan' , jumlah_orang = '$jumlah_orang' WHERE no_meja = '$no_meja'";
+    $result_update = $conn -> query($sql_update);
+
+    if($result_update) {
+        header("location: index.php");
+    } else {
+        $update_notifikasi = "Data gagal diupdate, silahkan coba lagi";
+    }
+    $conn->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,15 +49,18 @@ define("APP_NAME", "BOOKING MEJA")
 </head>
 
 <body>
+    <?php 
+        include "layouts/header.php";
+    ?>
     <div class="super-center">
-        <h1><?php echo APP_NAME ?></h1>
+        <h1><?= APP_NAME; echo $no_meja ?></h1>
         <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
 
             <label for="">Nama Pelanggan</label>
-            <input type="text" name="nama_pelanggan">
+            <input name="nama_pelanggan">
 
             <label for="">Jumlah Orang</label>
-            <input type="text" name="Jumlah Orang">
+            <input name="jumlah_orang">
 
             <button type="submit" name="btn-pesan">Pesan Meja</button>
         </form>
