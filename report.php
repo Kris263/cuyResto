ef5032260c50918629ca5b2c3ef69691e3007654
Kris263 <kristianptr2633@gmail.com>
2024-11-14 00:34:19 +0800

    Make Report

--- report.php
+++ report.php
@@ -4,6 +4,32 @@ include "services/koneksi.php";
 
 if(isset($_POST['btn-report'])) {
     $hari = $_POST['hari'];
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetTitle("Laporan Pengunjung");
+    $pdf->SetFont("Arial", "B", 14);
+    $pdf->Text(10, 6, "Laporan Pengunjung $hari");
+
+    $pdf->Cell(22, 10, "No Meja",1 , 0);
+    $pdf->Cell(44, 10, "Nama Pelanggan",1 , 0);
+    $pdf->Cell(38, 10, "Hari Keluar",1 , 0);
+    $pdf->Cell(38, 10, "Jam Keluar",1 , 0);
+    $pdf->Cell(40, 10, "",0 , 1);
+
+    $sql_report = "SELECT * FROM history WHERE hari='$hari'";
+    $result_report = $conn->query($sql_report);
+    if ($result_report->num_rows > 0) {
+        foreach ($result_report as $report) {
+            $pdf->Cell(22, 10, $report["no_meja"], 1, 0);
+            $pdf->Cell(44, 10, $report["nama_pelanggan"],1 , 0);
+            $pdf->Cell(38, 10, $report["hari"],1 , 0);
+            $pdf->Cell(38, 10, $report["jam"],1 , 1);
+        }
+    } else {
+        $pdf->Cell(0,10,"Tidak ada pelanggan hari ini", 0 ,1);
+    }
+
+    $pdf->Output();
 }
 ?>
 
